2d57fdab136c60078cbc8d921728f9e38f94767a
SajawalHassan <scientificninja1f12@gmail.com>
2021-09-05 20:08:25 +0500

    Added the bottom part of the header

--- src/Header.js
+++ src/Header.js
@@ -9,29 +9,97 @@ import VideoCallIcon from "@material-ui/icons/VideoCall";
 import AppsIcon from "@material-ui/icons/Apps";
 import NotificationsIcon from "@material-ui/icons/Notifications";
 
-import { Avatar } from "@material-ui/core";
+import { Avatar, Button } from "@material-ui/core";
 
 function Header() {
   return (
     <div className="header">
-      <div className="header__left">
-        <MenuIcon className="header__leftIconMenu" />
-        <div className="header__leftLogo">
-          <img src={YoutubeLogo} alt="" />
+      <div className="header__top">
+        <div className="header__topLeft">
+          <div className="header__topleftIconMenu">
+            <MenuIcon />
+          </div>
+          <div className="header__topLeftLogo">
+            <img src={YoutubeLogo} alt="" />
+          </div>
+        </div>
+
+        <div className="header__topMiddle">
+          <input type="text" placeholder="Search" />
+          <SearchIcon />
+        </div>
+
+        <div className="header__topRight">
+          <MicIcon />
+          <VideoCallIcon />
+          <AppsIcon />
+          <NotificationsIcon />
+          <Avatar className="header__topRightAvatar" />
         </div>
       </div>
 
-      <div className="header__middle">
-        <input type="text" placeholder="Search" />
-        <SearchIcon />
-      </div>
+      <div className="header__bottom">
+        <div className="header__bottomTags">
+          <div className="header__bottomTag active">
+            <p>All</p>
+          </div>
 
-      <div className="header__right">
-        <MicIcon />
-        <VideoCallIcon />
-        <AppsIcon />
-        <NotificationsIcon />
-        <Avatar className="header__rightAvatar" />
+          <div className="header__bottomTag">
+            <p>All</p>
+          </div>
+
+          <div className="header__bottomTag">
+            <p>All</p>
+          </div>
+
+          <div className="header__bottomTag">
+            <p>All</p>
+          </div>
+
+          <div className="header__bottomTag">
+            <p>All</p>
+          </div>
+
+          <div className="header__bottomTag">
+            <p>All</p>
+          </div>
+
+          <div className="header__bottomTag">
+            <p>All</p>
+          </div>
+
+          <div className="header__bottomTag">
+            <p>All</p>
+          </div>
+
+          <div className="header__bottomTag">
+            <p>All</p>
+          </div>
+
+          <div className="header__bottomTag">
+            <p>All</p>
+          </div>
+
+          <div className="header__bottomTag">
+            <p>All</p>
+          </div>
+
+          <div className="header__bottomTag">
+            <p>All</p>
+          </div>
+
+          <div className="header__bottomTag">
+            <p>All</p>
+          </div>
+
+          <div className="header__bottomTag">
+            <p>All</p>
+          </div>
+
+          <div className="header__bottomTag">
+            <p>All</p>
+          </div>
+        </div>
       </div>
     </div>
   );
