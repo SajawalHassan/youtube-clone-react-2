c9586f980c8a3219209df236ec837e74b20bdb24
SajawalHassan <scientificninja1f12@gmail.com>
2021-09-08 11:49:10 +0500

    Beutified the clone

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -11,6 +11,13 @@ import VideoLibraryIcon from "@material-ui/icons/VideoLibrary";
 import WatchLaterIcon from "@material-ui/icons/WatchLater";
 import AddPhotoAlternateIcon from "@material-ui/icons/AddPhotoAlternate";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import VideogameAssetIcon from "@material-ui/icons/VideogameAsset";
+import LiveTvIcon from "@material-ui/icons/LiveTv";
+import SportsIcon from "@material-ui/icons/Sports";
+import SettingsIcon from "@material-ui/icons/Settings";
+import FlagIcon from "@material-ui/icons/Flag";
+import HelpIcon from "@material-ui/icons/Help";
+import FeedbackIcon from "@material-ui/icons/Feedback";
 
 import Sidebar_Subscription_img_1 from "./Images/Sidebar_Subscription_img_1.jpg";
 import Sidebar_Subscription_img_2 from "./Images/Sidebar_Subscription_img_2.jpg";
@@ -23,101 +30,135 @@ import Sidebar_Subscription_img_7 from "./Images/Sidebar_Subscription_img_7.jpg"
 function Sidebar() {
   return (
     <div className="sidebar">
-      <div className="sidebar__top">
-        <div className="sidebar__Option sidebar__active">
-          <HomeIcon />
-          <SidebarOptions title="Home" />
-        </div>
+      <div title="Home" className="sidebar__Option sidebar__active">
+        <HomeIcon />
+        <SidebarOptions title="Home" />
+      </div>
 
-        <div className="sidebar__Option">
-          <ExploreIcon />
-          <SidebarOptions title="Explore" />
-        </div>
+      <div title="Explore" className="sidebar__Option">
+        <ExploreIcon />
+        <SidebarOptions title="Explore" />
+      </div>
 
-        <div className="sidebar__Option">
-          <SubscriptionsIcon />
-          <SidebarOptions title="Subscriptions" />
-        </div>
+      <div title="Subcription" className="sidebar__Option">
+        <SubscriptionsIcon />
+        <SidebarOptions title="Subscriptions" />
       </div>
 
       <div className="border"></div>
 
-      <div className="sidebar__middle">
-        <div className="sidebar__Option">
-          <LibraryAddIcon />
-          <SidebarOptions title="Library" />
-        </div>
+      <div title="Library" className="sidebar__Option">
+        <LibraryAddIcon />
+        <SidebarOptions title="Library" />
+      </div>
 
-        <div className="sidebar__Option">
-          <HistoryIcon />
-          <SidebarOptions title="History" />
-        </div>
+      <div title="History" className="sidebar__Option">
+        <HistoryIcon />
+        <SidebarOptions title="History" />
+      </div>
 
-        <div className="sidebar__Option">
-          <VideoLibraryIcon />
-          <SidebarOptions title="Your Videos" />
-        </div>
+      <div title="Your Videos" className="sidebar__Option">
+        <VideoLibraryIcon />
+        <SidebarOptions title="Your Videos" />
+      </div>
 
-        <div className="sidebar__Option">
-          <WatchLaterIcon />
-          <SidebarOptions title="Watch Later" />
-        </div>
+      <div title="Watch Later" className="sidebar__Option">
+        <WatchLaterIcon />
+        <SidebarOptions title="Watch Later" />
+      </div>
 
-        <div className="sidebar__Option">
-          <AddPhotoAlternateIcon />
-          <SidebarOptions title="Your Clips" />
-        </div>
+      <div title="Your Clips" className="sidebar__Option">
+        <AddPhotoAlternateIcon />
+        <SidebarOptions title="Your Clips" />
+      </div>
 
-        <div className="sidebar__Option">
-          <ExpandMoreIcon />
-          <SidebarOptions title="Show More" />
-        </div>
+      <div title="Show More" className="sidebar__Option">
+        <ExpandMoreIcon />
+        <SidebarOptions title="Show More" />
       </div>
 
       <div className="border"></div>
 
-      <div className="sidebar__bottom">
-        <h4>Subsciption</h4>
+      <h4>SUBSCRIPTIONS</h4>
 
-        <div className="sidebar__Option">
-          <img src={Sidebar_Subscription_img_1} alt="" />
-          <SidebarOptions title="Edureka!" />
-        </div>
+      <div title="Edureka!" className="sidebar__Option">
+        <img src={Sidebar_Subscription_img_1} alt="" />
+        <SidebarOptions title="Edureka!" />
+      </div>
 
-        <div className="sidebar__Option">
-          <img src={Sidebar_Subscription_img_2} alt="" />
-          <SidebarOptions title="EmKay" />
-        </div>
+      <div title="EmKay" className="sidebar__Option">
+        <img src={Sidebar_Subscription_img_2} alt="" />
+        <SidebarOptions title="EmKay" />
+      </div>
 
-        <div className="sidebar__Option">
-          <img src={Sidebar_Subscription_img_3} alt="" />
-          <SidebarOptions title="TheOdds1sOut" />
-        </div>
+      <div title="TheOddS1sOut" className="sidebar__Option">
+        <img src={Sidebar_Subscription_img_3} alt="" />
+        <SidebarOptions title="TheOdds1sOut" />
+      </div>
 
-        <div className="sidebar__Option">
-          <img src={Sidebar_Subscription_img_4} alt="" />
-          <SidebarOptions title="Micheal Reeves" />
-        </div>
+      <div title="Micheal Reeves" className="sidebar__Option">
+        <img src={Sidebar_Subscription_img_4} alt="" />
+        <SidebarOptions title="Micheal Reeves" />
+      </div>
 
-        <div className="sidebar__Option">
-          <img src={Sidebar_Subscription_img_5} alt="" />
-          <SidebarOptions title="Skeppy" />
-        </div>
+      <div title="Skeppy" className="sidebar__Option">
+        <img src={Sidebar_Subscription_img_5} alt="" />
+        <SidebarOptions title="Skeppy" />
+      </div>
 
-        <div className="sidebar__Option">
-          <img src={Sidebar_Subscription_img_6} alt="" />
-          <SidebarOptions title="Pewdiepie" />
-        </div>
+      <div title="Pewdiepie" className="sidebar__Option">
+        <img src={Sidebar_Subscription_img_6} alt="" />
+        <SidebarOptions title="Pewdiepie" />
+      </div>
 
-        <div className="sidebar__Option">
-          <img src={Sidebar_Subscription_img_7} alt="" />
-          <SidebarOptions title="Pegasus" />
-        </div>
+      <div title="Pegasus" className="sidebar__Option">
+        <img src={Sidebar_Subscription_img_7} alt="" />
+        <SidebarOptions title="Pegasus" />
+      </div>
 
-        <div className="sidebar__Option">
-          <ExpandMoreIcon />
-          <SidebarOptions title="Show 89 More" />
-        </div>
+      <div title="Show 89 More" className="sidebar__Option">
+        <ExpandMoreIcon />
+        <SidebarOptions title="Show 89 More" />
+      </div>
+
+      <div className="border"></div>
+
+      <h4>MORE FROM YOUTUBE</h4>
+      <div title="Gaming" className="sidebar__Option">
+        <VideogameAssetIcon />
+        <SidebarOptions title="Gaming" />
+      </div>
+
+      <div title="Live" className="sidebar__Option">
+        <LiveTvIcon />
+        <SidebarOptions title="Live" />
+      </div>
+
+      <div title="Sports" className="sidebar__Option">
+        <SportsIcon />
+        <SidebarOptions title="Sports" />
+      </div>
+
+      <div className="border"></div>
+
+      <div title="Settings" className="sidebar__Option">
+        <SettingsIcon />
+        <SidebarOptions title="Settings" />
+      </div>
+
+      <div title="Report history" className="sidebar__Option">
+        <FlagIcon />
+        <SidebarOptions title="Report history" />
+      </div>
+
+      <div title="Help" className="sidebar__Option">
+        <HelpIcon />
+        <SidebarOptions title="Help" />
+      </div>
+
+      <div title="Send Feedback" className="sidebar__Option last">
+        <FeedbackIcon />
+        <SidebarOptions title="Send Feedback" />
       </div>
     </div>
   );
--- src/Video.js
+++ src/Video.js
@@ -8,275 +8,263 @@ import channelImg1 from "./Images/channelImg-1.jpg";
 function Video() {
   return (
     <div className="video">
-      <VideoCard
-        img={Thumbnail1}
-        title="How to make awesome avatars"
-        channelImg={channelImg1}
-        channelTitle="fireship"
-        views="362K views •"
-        date="3 weeks ago"
-      />
+      <div className="video__top">
+        <VideoCard
+          img={Thumbnail1}
+          title="How to make awesome avatars"
+          channelImg={channelImg1}
+          channelTitle="fireship"
+          views="362K views •"
+          date="3 weeks ago"
+        />
 
-      <VideoCard
-        img={Thumbnail1}
-        title="How to make awesome avatars"
-        channelImg={channelImg1}
-        channelTitle="fireship"
-        views="362K views •"
-        date="3 weeks ago"
-      />
+        <VideoCard
+          img={Thumbnail1}
+          title="How to make awesome avatars"
+          channelImg={channelImg1}
+          channelTitle="fireship"
+          views="362K views •"
+          date="3 weeks ago"
+        />
 
-      <VideoCard
-        img={Thumbnail1}
-        title="How to make awesome avatars"
-        channelImg={channelImg1}
-        channelTitle="fireship"
-        views="362K views •"
-        date="3 weeks ago"
-      />
+        <VideoCard
+          img={Thumbnail1}
+          title="How to make awesome avatars"
+          channelImg={channelImg1}
+          channelTitle="fireship"
+          views="362K views •"
+          date="3 weeks ago"
+        />
 
-      <VideoCard
-        img={Thumbnail1}
-        title="How to make awesome avatars"
-        channelImg={channelImg1}
-        channelTitle="fireship"
-        views="362K views •"
-        date="3 weeks ago"
-      />
+        <VideoCard
+          img={Thumbnail1}
+          title="How to make awesome avatars"
+          channelImg={channelImg1}
+          channelTitle="fireship"
+          views="362K views •"
+          date="3 weeks ago"
+        />
 
-      <VideoCard
-        img={Thumbnail1}
-        title="How to make awesome avatars"
-        channelImg={channelImg1}
-        channelTitle="fireship"
-        views="362K views •"
-        date="3 weeks ago"
-      />
+        <VideoCard
+          img={Thumbnail1}
+          title="How to make awesome avatars"
+          channelImg={channelImg1}
+          channelTitle="fireship"
+          views="362K views •"
+          date="3 weeks ago"
+        />
 
-      <VideoCard
-        img={Thumbnail1}
-        title="How to make awesome avatars"
-        channelImg={channelImg1}
-        channelTitle="fireship"
-        views="362K views •"
-        date="3 weeks ago"
-      />
+        <VideoCard
+          img={Thumbnail1}
+          title="How to make awesome avatars"
+          channelImg={channelImg1}
+          channelTitle="fireship"
+          views="362K views •"
+          date="3 weeks ago"
+        />
 
-      <VideoCard
-        img={Thumbnail1}
-        title="How to make awesome avatars"
-        channelImg={channelImg1}
-        channelTitle="fireship"
-        views="362K views •"
-        date="3 weeks ago"
-      />
+        <VideoCard
+          img={Thumbnail1}
+          title="How to make awesome avatars"
+          channelImg={channelImg1}
+          channelTitle="fireship"
+          views="362K views •"
+          date="3 weeks ago"
+        />
 
-      <VideoCard
-        img={Thumbnail1}
-        title="How to make awesome avatars"
-        channelImg={channelImg1}
-        channelTitle="fireship"
-        views="362K views •"
-        date="3 weeks ago"
-      />
+        <VideoCard
+          img={Thumbnail1}
+          title="How to make awesome avatars"
+          channelImg={channelImg1}
+          channelTitle="fireship"
+          views="362K views •"
+          date="3 weeks ago"
+        />
+      </div>
 
-      <VideoCard
-        img={Thumbnail1}
-        title="How to make awesome avatars"
-        channelImg={channelImg1}
-        channelTitle="fireship"
-        views="362K views •"
-        date="3 weeks ago"
-      />
+      <div className="video__middle">
+        <VideoCard
+          img={Thumbnail1}
+          title="How to make awesome avatars"
+          channelImg={channelImg1}
+          channelTitle="fireship"
+          views="362K views •"
+          date="3 weeks ago"
+        />
 
-      <VideoCard
-        img={Thumbnail1}
-        title="How to make awesome avatars"
-        channelImg={channelImg1}
-        channelTitle="fireship"
-        views="362K views •"
-        date="3 weeks ago"
-      />
+        <VideoCard
+          img={Thumbnail1}
+          title="How to make awesome avatars"
+          channelImg={channelImg1}
+          channelTitle="fireship"
+          views="362K views •"
+          date="3 weeks ago"
+        />
 
-      <VideoCard
-        img={Thumbnail1}
-        title="How to make awesome avatars"
-        channelImg={channelImg1}
-        channelTitle="fireship"
-        views="362K views •"
-        date="3 weeks ago"
-      />
+        <VideoCard
+          img={Thumbnail1}
+          title="How to make awesome avatars"
+          channelImg={channelImg1}
+          channelTitle="fireship"
+          views="362K views •"
+          date="3 weeks ago"
+        />
 
-      <VideoCard
-        img={Thumbnail1}
-        title="How to make awesome avatars"
-        channelImg={channelImg1}
-        channelTitle="fireship"
-        views="362K views •"
-        date="3 weeks ago"
-      />
+        <VideoCard
+          img={Thumbnail1}
+          title="How to make awesome avatars"
+          channelImg={channelImg1}
+          channelTitle="fireship"
+          views="362K views •"
+          date="3 weeks ago"
+        />
 
-      <VideoCard
-        img={Thumbnail1}
-        title="How to make awesome avatars"
-        channelImg={channelImg1}
-        channelTitle="fireship"
-        views="362K views •"
-        date="3 weeks ago"
-      />
+        <VideoCard
+          img={Thumbnail1}
+          title="How to make awesome avatars"
+          channelImg={channelImg1}
+          channelTitle="fireship"
+          views="362K views •"
+          date="3 weeks ago"
+        />
 
-      <VideoCard
-        img={Thumbnail1}
-        title="How to make awesome avatars"
-        channelImg={channelImg1}
-        channelTitle="fireship"
-        views="362K views •"
-        date="3 weeks ago"
-      />
+        <VideoCard
+          img={Thumbnail1}
+          title="How to make awesome avatars"
+          channelImg={channelImg1}
+          channelTitle="fireship"
+          views="362K views •"
+          date="3 weeks ago"
+        />
 
-      <VideoCard
-        img={Thumbnail1}
-        title="How to make awesome avatars"
-        channelImg={channelImg1}
-        channelTitle="fireship"
-        views="362K views •"
-        date="3 weeks ago"
-      />
+        <VideoCard
+          img={Thumbnail1}
+          title="How to make awesome avatars"
+          channelImg={channelImg1}
+          channelTitle="fireship"
+          views="362K views •"
+          date="3 weeks ago"
+        />
 
-      <VideoCard
-        img={Thumbnail1}
-        title="How to make awesome avatars"
-        channelImg={channelImg1}
-        channelTitle="fireship"
-        views="362K views •"
-        date="3 weeks ago"
-      />
+        <VideoCard
+          img={Thumbnail1}
+          title="How to make awesome avatars"
+          channelImg={channelImg1}
+          channelTitle="fireship"
+          views="362K views •"
+          date="3 weeks ago"
+        />
+      </div>
 
-      <VideoCard
-        img={Thumbnail1}
-        title="How to make awesome avatars"
-        channelImg={channelImg1}
-        channelTitle="fireship"
-        views="362K views •"
-        date="3 weeks ago"
-      />
+      <div className="video__bottom">
+        <VideoCard
+          img={Thumbnail1}
+          title="How to make awesome avatars"
+          channelImg={channelImg1}
+          channelTitle="fireship"
+          views="362K views •"
+          date="3 weeks ago"
+        />
 
-      <VideoCard
-        img={Thumbnail1}
-        title="How to make awesome avatars"
-        channelImg={channelImg1}
-        channelTitle="fireship"
-        views="362K views •"
-        date="3 weeks ago"
-      />
+        <VideoCard
+          img={Thumbnail1}
+          title="How to make awesome avatars"
+          channelImg={channelImg1}
+          channelTitle="fireship"
+          views="362K views •"
+          date="3 weeks ago"
+        />
 
-      <VideoCard
-        img={Thumbnail1}
-        title="How to make awesome avatars"
-        channelImg={channelImg1}
-        channelTitle="fireship"
-        views="362K views •"
-        date="3 weeks ago"
-      />
+        <VideoCard
+          img={Thumbnail1}
+          title="How to make awesome avatars"
+          channelImg={channelImg1}
+          channelTitle="fireship"
+          views="362K views •"
+          date="3 weeks ago"
+        />
 
-      <VideoCard
-        img={Thumbnail1}
-        title="How to make awesome avatars"
-        channelImg={channelImg1}
-        channelTitle="fireship"
-        views="362K views •"
-        date="3 weeks ago"
-      />
+        <VideoCard
+          img={Thumbnail1}
+          title="How to make awesome avatars"
+          channelImg={channelImg1}
+          channelTitle="fireship"
+          views="362K views •"
+          date="3 weeks ago"
+        />
 
-      <VideoCard
-        img={Thumbnail1}
-        title="How to make awesome avatars"
-        channelImg={channelImg1}
-        channelTitle="fireship"
-        views="362K views •"
-        date="3 weeks ago"
-      />
+        <VideoCard
+          img={Thumbnail1}
+          title="How to make awesome avatars"
+          channelImg={channelImg1}
+          channelTitle="fireship"
+          views="362K views •"
+          date="3 weeks ago"
+        />
 
-      <VideoCard
-        img={Thumbnail1}
-        title="How to make awesome avatars"
-        channelImg={channelImg1}
-        channelTitle="fireship"
-        views="362K views •"
-        date="3 weeks ago"
-      />
+        <VideoCard
+          img={Thumbnail1}
+          title="How to make awesome avatars"
+          channelImg={channelImg1}
+          channelTitle="fireship"
+          views="362K views •"
+          date="3 weeks ago"
+        />
 
-      <VideoCard
-        img={Thumbnail1}
-        title="How to make awesome avatars"
-        channelImg={channelImg1}
-        channelTitle="fireship"
-        views="362K views •"
-        date="3 weeks ago"
-      />
+        <VideoCard
+          img={Thumbnail1}
+          title="How to make awesome avatars"
+          channelImg={channelImg1}
+          channelTitle="fireship"
+          views="362K views •"
+          date="3 weeks ago"
+        />
 
-      <VideoCard
-        img={Thumbnail1}
-        title="How to make awesome avatars"
-        channelImg={channelImg1}
-        channelTitle="fireship"
-        views="362K views •"
-        date="3 weeks ago"
-      />
+        <VideoCard
+          img={Thumbnail1}
+          title="How to make awesome avatars"
+          channelImg={channelImg1}
+          channelTitle="fireship"
+          views="362K views •"
+          date="3 weeks ago"
+        />
 
-      <VideoCard
-        img={Thumbnail1}
-        title="How to make awesome avatars"
-        channelImg={channelImg1}
-        channelTitle="fireship"
-        views="362K views •"
-        date="3 weeks ago"
-      />
+        <VideoCard
+          img={Thumbnail1}
+          title="How to make awesome avatars"
+          channelImg={channelImg1}
+          channelTitle="fireship"
+          views="362K views •"
+          date="3 weeks ago"
+        />
 
-      <VideoCard
-        img={Thumbnail1}
-        title="How to make awesome avatars"
-        channelImg={channelImg1}
-        channelTitle="fireship"
-        views="362K views •"
-        date="3 weeks ago"
-      />
+        <VideoCard
+          img={Thumbnail1}
+          title="How to make awesome avatars"
+          channelImg={channelImg1}
+          channelTitle="fireship"
+          views="362K views •"
+          date="3 weeks ago"
+        />
 
-      <VideoCard
-        img={Thumbnail1}
-        title="How to make awesome avatars"
-        channelImg={channelImg1}
-        channelTitle="fireship"
-        views="362K views •"
-        date="3 weeks ago"
-      />
+        <VideoCard
+          img={Thumbnail1}
+          title="How to make awesome avatars"
+          channelImg={channelImg1}
+          channelTitle="fireship"
+          views="362K views •"
+          date="3 weeks ago"
+        />
 
-      <VideoCard
-        img={Thumbnail1}
-        title="How to make awesome avatars"
-        channelImg={channelImg1}
-        channelTitle="fireship"
-        views="362K views •"
-        date="3 weeks ago"
-      />
-
-      <VideoCard
-        img={Thumbnail1}
-        title="How to make awesome avatars"
-        channelImg={channelImg1}
-        channelTitle="fireship"
-        views="362K views •"
-        date="3 weeks ago"
-      />
-
-      <VideoCard
-        img={Thumbnail1}
-        title="How to make awesome avatars"
-        channelImg={channelImg1}
-        channelTitle="fireship"
-        views="362K views •"
-        date="3 weeks ago"
-      />
+        <VideoCard
+          img={Thumbnail1}
+          title="How to make awesome avatars"
+          channelImg={channelImg1}
+          channelTitle="fireship"
+          views="362K views •"
+          date="3 weeks ago"
+        />
+      </div>
     </div>
   );
 }
